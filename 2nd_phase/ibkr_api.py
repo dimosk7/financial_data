fd0517058ede8f52f3f14fe194d27dd08247de80
Dimos Kelepouris <99260680+dimosk7@users.noreply.github.com>
2022-06-06 23:48:31 +0300

    Update ibkr_api.py

--- 2nd_phase/ibkr_api.py
+++ 2nd_phase/ibkr_api.py
@@ -22,6 +22,7 @@ pd_data = pd.DataFrame(data)
 # select the columns we need
 col = ["execution_id", "account", "symbol", "trade_time", "side", "size", "price", "net_amount", "commission" ]
 sql_data = pd_data[col]
+sql_data.rename(columns = { "symbol" : "ticker", "trade_time" : "trade time", "net_amount": "net amount" }, inplace = True)
 
 # trade_time should be converted to the appropriate format (from str to datetime)
 sql_data["trade_time"] = sql_data["trade_time"].apply( lambda x : datetime.strptime(x, "%Y%m%d-%H:%M:%S"))
@@ -40,6 +41,7 @@ pd_data = pd.DataFrame(data)
 # select columns we need
 col = ["accountId", "accountTitle", "currency", "tradingType"]
 sql_data = pd_data[col]
+sql_data.rename(columns = {"accountId" : "account id", "accountTitle" : "account title", "tradingType" : "trading type"}, inplace = True)
 
 # create table "accounts"
 pySQL_functions.create_table(sql_data, "accounts", mydb)
